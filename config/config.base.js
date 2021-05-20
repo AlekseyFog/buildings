c2a527300d511acfcf5a81473ea570fa8531aeb7
Aleksey <AlekseyFog@github.com>
2021-05-20 19:54:38 +0300

    try to fix that fckng path to public!

--- config/config.base.js
+++ config/config.base.js
@@ -2,6 +2,6 @@ module.exports = {
   outputDir: 'docs',
   runtimeCompiler: false,
   productionSourceMap: false,
-  publicPath: process.env.NODE_ENV === 'production' ? '/buildings' : '',
+  publicPath: process.env.NODE_ENV === 'production' ? '/buildings/' : '',
   assetsDir: process.env.NODE_ENV === 'production' ? 'buildings/' : '',
 };
